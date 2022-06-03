79266eebf9caa8e896d65bfd968eb9121c801201
Artem Kuzmenko <arkuzka@mail.ru>
2022-06-03 18:35:18 +0300

    Перенес получение списка кандидатов во вьюхи

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ from utils import (load_candidates,
 
 app = Flask(__name__)
 
-candidates = load_candidates()
-
 
 @app.route("/")
 def index():
-    return get_formatted_candidates(candidates)
+    return get_formatted_candidates(load_candidates())
 
 
 @app.route("/candidates/<name>")
 def candidate_by_name(name):
-    candidate = get_candidate_by_name(candidates, name)
+    candidate = get_candidate_by_name(load_candidates(), name)
     if candidate is None:
         return f'В списке кандидатов не найден "{name}"'
     return get_formatted_candidate(candidate)
@@ -24,7 +22,7 @@ def candidate_by_name(name):
 
 @app.route("/skills/<skill>")
 def candidates_by_skill(skill):
-    return get_formatted_candidates(get_candidates_by_skill(candidates, skill))
+    return get_formatted_candidates(get_candidates_by_skill(load_candidates(), skill))
 
 
 if __name__ == '__main__':
