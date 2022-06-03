35c58989bbb9c1f470b1bcafd1f1748349cd9296
Artem Kuzmenko <arkuzka@mail.ru>
2022-06-03 14:53:48 +0300

    Написл тестовое приложение

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from utils import (load_candidates,
+                   get_candidate_by_name,
+                   get_candidates_by_skill,
+                   get_formatted_candidates, get_formatted_candidate)
+
+app = Flask(__name__)
+
+candidates = load_candidates()
+
+
+@app.route("/")
+def index():
+    return get_formatted_candidates(candidates)
+
+
+@app.route("/candidates/<name>")
+def candidate_by_name(name):
+    candidate = get_candidate_by_name(candidates, name)
+    if candidate is None:
+        return f'В списке кандидатов не найден "{name}"'
+    return get_formatted_candidate(candidate)
+
+
+@app.route("/skills/<skill>")
+def candidates_by_skill(skill):
+    return get_formatted_candidates(get_candidates_by_skill(candidates, skill))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,54 @@
+import json
+
+
+CANDIDATES_FILE = 'candidates.json'
+
+
+def _load_json_file(filename):
+    with open(filename) as file:
+        return json.load(file)
+
+
+def load_candidates():
+    return _load_json_file(CANDIDATES_FILE)
+
+
+def get_candidate_by_name(candidates, name):
+    for candidat in candidates:
+        if candidat['name'].lower() == name.lower():
+            return candidat
+    return None
+
+
+def get_candidates_by_skill(candidates, skill):
+    candidates_with_skill = []
+    for candidat in candidates:
+        if skill.lower() in candidat['skills'].lower():
+            candidates_with_skill.append(candidat)
+    return candidates_with_skill
+
+
+def get_formatted_candidates(candidates):
+    result_str = ''
+    for candidat in candidates:
+        result_str += f'''
+<pre>
+  ---------------------------------------
+  ФИО: {candidat["name"]}
+  Возраст: {candidat["age"]}, пол: {candidat["gender"]}
+  Должность: {candidat["position"]}
+  Навыки: {candidat["skills"]}
+
+<pre>'''
+    return result_str if result_str else 'Список кандидатов пуст'
+
+
+def get_formatted_candidate(candidate):
+    return f'''<img src="{candidate["picture"]}">
+<pre>
+  ---------------------------------------
+  ФИО: {candidate["name"]}
+  Возраст: {candidate["age"]}, пол: {candidate["gender"]}
+  Должность: {candidate["position"]}
+  Навыки: {candidate["skills"]}
+<pre>'''
